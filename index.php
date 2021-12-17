192747e2810e0c1de3ee8ea2ddb57ea7b1e2b5d1
Danial Gharib <90343552+Danial-Gharib@users.noreply.github.com>
2021-12-18 01:11:48 +0330

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
-$question = 'این یک پرسش نمونه است';
-$msg = 'این یک پاسخ نمونه است';
-$en_name = 'hafez';
-$fa_name = 'حافظ';
+$question = $_POST['question'];
+$messages = explode("\n", file_get_contents('./messages.txt'));
+$people = file_get_contents('./people.json');
+$json_people = json_decode($people, true);
+if (is_null($_POST['person'])) {
+    $en_name = array_rand($json_people);
+    $fa_name = $json_people[$en_name];
+} else {
+    $en_name = $_POST['person'];
+    $fa_name = $json_people[$en_name];
+}
+if (empty($question)) {
+    $msg = 'سوال خود را بپرس!';
+}
+else {
+    $msg = $messages[hexdec(substr(md5($question . $en_name), 0, 15)) % count($messages)];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,7 +28,10 @@ $fa_name = 'حافظ';
 <p id="copyright">تهیه شده برای درس کارگاه کامپیوتر،دانشکده کامییوتر، دانشگاه صنعتی شریف</p>
 <div id="wrapper">
     <div id="title">
-        <span id="label">پرسش:</span>
+        <span
+            id="label"
+            style="display: <?php echo empty($question) ? 'none' : 'inline' ?>;"
+        >پرسش:</span>
         <span id="question"><?php echo $question ?></span>
     </div>
     <div id="container">
@@ -36,11 +52,9 @@ $fa_name = 'حافظ';
             را از
             <select name="person">
                 <?php
-                /*
-                 * Loop over people data and
-                 * enter data inside `option` tag.
-                 * E.g., <option value="hafez">حافظ</option>
-                 */
+                foreach ($json_people as $key => $value) {
+                    echo '<option name="" value="'.$key.'" '. ($en_name == $key ? 'selected' : '') .'>'.$value.'</option>';
+                }
                 ?>
             </select>
             <input type="submit" value="بپرس"/>
